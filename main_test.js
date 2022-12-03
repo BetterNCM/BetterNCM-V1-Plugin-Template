df6a354295d1a70f4b2a7c1f5b723cd34a475d9d
MicroCBer <66859419+MicroCBer@users.noreply.github.com>
2022-12-03 16:08:18 +0800

    Update main_test.js

--- main_test.js
+++ main_test.js
@@ -1,10 +1,20 @@
 let successfully_hooked=false;
-window.expose=()=>{
-    successfully_hooked=true;
+window.expose=(a)=>{
+    successfully_hooked=JSON.stringify(a.lrc);
 }
 
 document.querySelector(".ply").click();
 
+// polyfill for <0.2.6
+betterncm.tests={
+    async fail(reason) {
+        await betterncm.fs.writeFileText("/__TEST_FAILED__.txt", reason);
+    }, async success(message) {
+        await betterncm.fs.writeFileText("/__TEST_SUCCEEDED__.txt", message);
+    }
+}
+
 setTimeout(()=>{
     if(!successfully_hooked)betterncm.tests.fail("Hijack lyrics timeout");
+    else betterncm.tests.success(successfully_hooked)
 },5000)
